8280d5f00929747720c767388addd90105bc8003
Sumit Saurabh <62152915+sumitsaurabh927@users.noreply.github.com>
2023-10-18 23:42:01 +0530

    refactor: add style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import axios from 'axios'
 import hero from './assets/hero.png'
-
+import './app.css'
 const App = () => {
 
   const [chatMsg, setChatMsg] = useState('')
@@ -16,29 +16,28 @@ const App = () => {
 
   }
   return (
-    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', minHeight: '100vh', fontFamily: 'Montserrat, sans-serif' }}>
-      <div style={{ width: '50%', flex: 1 }}>
-        <img src={hero} style={{ width: '800px' }} />
+    <div className="container">
+      <div className="image-container">
+        <img src={hero} className="image" alt="Hero" />
       </div>
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '50%' }}>
-        <form onSubmit={onSubmitHandler} style={{ marginBottom: '5rem' }}>
+      <div className="form-container">
+        <form onSubmit={onSubmitHandler}>
           <h1>Send a Slack notification!</h1>
           <div>
-            <p>
-              Use Novu to send notifications directly to a channel
-            </p>
-            <p>
-              in a Slack workspace.
-            </p>
-            <p>
-              Get started <a href='https://docs.novu.co/channels-and-providers/chat/slack'> here!</a>
-            </p>
+            <p>Use Novu to send notifications directly to a channel</p>
+            <p>in a Slack workspace.</p>
+            <p>Get started <a href="https://docs.novu.co/channels-and-providers/chat/slack">here!</a></p>
           </div>
-          <input value={chatMsg} onChange={onChangeHandler} style={{ marginRight: '1rem', padding: '0.2rem' }} placeholder='Enter notification text' />
-          <button style={{ padding: '0.2rem 1rem' }}>Send</button>
+          <input
+            value={chatMsg}
+            onChange={onChangeHandler}
+            className="input-field"
+            placeholder="Enter notification text"
+          />
+          <button className="button">Send</button>
         </form>
       </div>
-    </div >
+    </div>
   )
 }
 
